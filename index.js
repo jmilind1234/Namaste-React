dbfd84825b2cd194f8f96420d0bcdbcbdf83c613
Milind Jain <jmilind1234@gmail.com>
2024-02-24 21:21:12 +0530

    removed cdn react links

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const heading1 = React.createElement("h1", { id: "heading1" }, "Hello world!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading1);
