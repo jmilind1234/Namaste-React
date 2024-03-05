7cb4d48feab5690e2e6d06e266cbe839e0ff7bd5
Milind Jain <jmilind1234@gmail.com>
2024-03-05 11:01:44 +0530

    added to filter out restaurants having avgRating more than 4.5

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,35 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurants } from "../data/const";
+import { useState } from "react";
 
 const Body = () => {
+  const [topRatedFlag, setTopRatedFlag] = useState(false);
   return (
     <div className="body">
       <div className="search">Search</div>
-      <div className="restaurant-container">
-        {restaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
-        ))}
-      </div>
+      <button
+        onClick={() => {
+          setTopRatedFlag(true);
+        }}
+      >
+        Top rated restaurants
+      </button>
+      {!topRatedFlag && (
+        <div className="restaurant-container">
+          {restaurants.map((restaurant) => (
+            <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
+          ))}
+        </div>
+      )}
+      {topRatedFlag && (
+        <div className="restaurant-container">
+          {restaurants
+            ?.filter((restaurant) => restaurant?.info?.avgRating > 4.5)
+            .map((restaurant) => (
+              <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
+            ))}
+        </div>
+      )}
     </div>
   );
 };
