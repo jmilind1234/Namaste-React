11eb6979f628c617316372532bd625c26734915f
Milind Jain <jmilind1234@gmail.com>
2024-03-06 13:54:28 +0530

    added logout and login button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
+import { useState } from "react";
 import { platformLogo } from "../data/const";
 
 const Header = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src={platformLogo}
-        ></img>
+        <img className="logo" src={platformLogo}></img>
       </div>
       <div className="nav-items">
         <ul>
@@ -15,6 +14,11 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <li>
+            <button onClick={() => setLoggedIn(!loggedIn)}>
+              {loggedIn ? "LOGOUT" : "LOGIN"}
+            </button>
+          </li>
         </ul>
       </div>
     </div>
