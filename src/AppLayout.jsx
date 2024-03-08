a04ea0ff4d03d71771d68bd5956263bdadf293d3
Milind Jain <jmilind1234@gmail.com>
2024-03-08 09:55:32 +0530

    removed the router configuration

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,9 +1,5 @@
 import Body from "./components/Body";
 import Header from "./components/Header";
-import About from "./components/About";
-import { createBrowserRouter } from "react-router-dom";
-import Contact from "./components/Contact";
-import Error from "./components/Error";
 
 const AppLayout = () => {
   return (
